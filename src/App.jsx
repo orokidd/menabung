d86776dbc13e9a4e50f52556f1bd60b9255927ac
orokidd <gregomily@gmail.com>
2026-01-11 14:38:15 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const BOX_COUNT = 550;
+
+function App() {
+  const stored = localStorage.getItem("savings");
+
+	const initialDays = Array.from({ length: BOX_COUNT }, (_, i) => ({
+		id: i,
+		saved: false,
+	}));
+
+	const [days, setDays] = useState(stored ? JSON.parse(stored) : initialDays);
+
+	useEffect(() => {
+		localStorage.setItem("savings", JSON.stringify(days));
+	}, [days]);
+
+	const toggleDay = (index) => {
+    const newDays = [...days];
+    newDays[index].saved = !newDays[index].saved;
+    setDays(newDays);
+  }
+
+	const savedCount = days.filter((day) => day.saved).length;
+  const totalAmount = savedCount * 20000;
+
+	return (
+		<div className="app">
+			<h1>Savings Tracker</h1>
+			<p>
+				Total: Rp {totalAmount.toLocaleString()}
+			</p>
+
+      <div className="reset">
+        <button onClick={() => {
+          localStorage.removeItem("savings");
+          setDays(initialDays);
+        }}>
+          Reset Savings
+        </button>
+      </div>
+
+			<div className="grid">
+				{days.map((day, index) => (
+					<div key={index} className={`box ${day.saved ? "saved" : ""}`} onClick={() => toggleDay(index)}>
+						20k
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default App;
